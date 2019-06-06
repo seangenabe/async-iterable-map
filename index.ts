8a4fe252662b4f5e9ecba2da3d74cc33d6ec5170
Sean Genabe <seanmarviolivergenabe@gmail.com>
2019-06-06 10:18:39 +0800

    init

---
+++ index.ts
@@ -0,0 +1,70 @@
+import { Semaphore } from "@seangenabe/semaphore"
+
+class AMapProcessor<T, U>
+  implements AsyncIterableIterator<U>, PromiseLike<U[]> {
+  private semaphore: Semaphore
+  private sourceIterator: AsyncIterator<T> | Iterator<T>
+
+  constructor(
+    source: AsyncIterable<T> | Iterable<T>,
+    private transform: (element: T) => Promise<U> | U,
+    { concurrency = Infinity }: AMapOptions = {}
+  ) {
+    this.semaphore = new Semaphore(concurrency)
+    this.sourceIterator = isAsyncIterable(source)
+      ? source[Symbol.asyncIterator]()
+      : source[Symbol.iterator]()
+  }
+
+  async next(): Promise<IteratorResult<U>> {
+    const { done, value } = await this.sourceIterator.next()
+    if (done) {
+      return { done: true } as IteratorResult<U>
+    }
+    const release = await this.semaphore.wait()
+    const result = await this.transform(value)
+    release()
+    return { done: false, value: result }
+  }
+
+  [Symbol.asyncIterator]() {
+    return this
+  }
+
+  private async toArray() {
+    const out: U[] = []
+    for await (const element of this) {
+      out.push(element)
+    }
+    return out
+  }
+
+  then<TResult1 = U[], TResult2 = never>(
+    onfulfilled?:
+      | ((value: U[]) => TResult1 | PromiseLike<TResult1>)
+      | null
+      | undefined,
+    onrejected?:
+      | ((reason: any) => TResult2 | PromiseLike<TResult2>)
+      | null
+      | undefined
+  ): PromiseLike<TResult1 | TResult2> {
+    return this.toArray().then(onfulfilled, onrejected)
+  }
+}
+
+export function aMap<T = unknown, U = unknown>(
+  source: AsyncIterable<T> | Iterable<T>,
+  transform: (element: T) => Promise<U> | U,
+  options: AMapOptions = {}
+): AsyncIterableIterator<U> & PromiseLike<U[]> {
+  return new AMapProcessor(source, transform, options)
+}
+
+export interface AMapOptions {
+  concurrency?: number
+}
+
+function isAsyncIterable(x: any): x is AsyncIterable<unknown> {
+  return typeof x[Symbol.asyncIterator] === "function"
+}
---
+++ readme.md
@@ -0,0 +1,33 @@
+# a-map
+
+Transforms items of an async iterable concurrently
+
+Like `p-map` but for `AsyncIterable`s.
+
+If you like this package, be sure to star its repo, and please consider [donating](https://seangenabe.netlify.com/donate).
+
+## Usage
+
+```typescript
+import { aMap } from "a-map"
+```
+
+### aMap(source, transform, options = {})
+
+Generic type parameters:
+* `T = unknown` - type of input elements
+* `U = unknown` - type of output elements
+
+Parameters:
+* `source: AsyncIterable<T> | Iterable<T>` - the iterable to transform
+* `transform: (element: T) => Promise<U> | U` - the transform / mapping function from the input to the output
+* `options.concurrency: number` - how many elements to transform concurrently. Must be a positive integer or `Infinity`. Default: `Infinity`
+
+Returns:
+* `AsyncIterableIterator<U> & PromiseLike<U[]>`
+  * Use the `AsyncIterableIterator<U>` interface to iterate through the remaining output elements one by one.
+  * Use the `PromiseLike<U[]>` interface to get all of the remaining output elements.
+
+Both interfaces return the output elements in order.
+
+Transforms items of an async iterable concurrently.
